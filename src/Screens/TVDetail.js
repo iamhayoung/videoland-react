708935ae87f97d9000a0f432f66bb139de8f1cc4
Hayoung Gam <hahhyoung@gmail.com>
2021-04-11 21:07:24 +0900

    Create TV Detail js

--- src/Screens/TVDetail.js
+++ src/Screens/TVDetail.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { tvApi } from "api";
+import { Helmet } from "react-helmet"
+import Loader from "Components/Loader";
 
 function TVDetail() {
+  const [isLoading, setLoading] = useState(true);
+
+  // async function getMoviesData() {
+  //   const a = await moviesApi.nowPlaying();
+  //   console.log(a)
+  // }
+
+  useEffect(() => {
+    try {
+      // getMoviesData()
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false);
+    }
+  }, [])
+
   return (
-    <p>TVDetail</p>
+    <>
+      <Helmet>
+        <title>hoge | TV Shows | Videoland</title>
+      </Helmet>
+      {(isLoading) ? <Loader /> : (
+        <p>TV Detail</p>
+      )}
+    </>
   )
 }
 
