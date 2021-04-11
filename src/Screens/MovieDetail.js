e241661ae59212c87443fc8ecbc7b0d4596ed4fc
Hayoung Gam <hahhyoung@gmail.com>
2021-04-12 01:02:09 +0900

    Create MovieDetail js, set background

--- src/Screens/MovieDetail.js
+++ src/Screens/MovieDetail.js
@@ -1,19 +1,61 @@
 import React, { useState, useEffect } from "react";
 import { moviesApi } from "api";
 import { Helmet } from "react-helmet"
+import styled from "styled-components";
 import Loader from "Components/Loader";
+import Message from "Components/Message";
 
-function MovieDetail(props) {
+const Container = styled.section`
+  width: 100%;
+  height: calc(100vh - 60px);
+  color: #fff;
+`;
+
+const Background = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-image: url(${ props => props.imgUrl});
+  background-position: center;
+  background-size: cover;
+  filter: blur(3px);
+  opacity: 0.5;
+  z-index: 0;
+`;
+
+function MovieDetail({ match: { params: { id }} }) {
   const [isLoading, setLoading] = useState(true);
-  console.log(props)
+  const [error, setError] = useState(null);
+  const [detailInfo, setDetailInfo] = useState({})
+  console.log(detailInfo)
+
+  async function getMovieDetail() {
+    try {
+      const { data } = await moviesApi.movieDetail(id);
+      setDetailInfo(data)
+    } catch {
+      setError("Can't find results.");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getMovieDetail()
+  }, [])
 
   return (
     <>
       <Helmet>
-        <title>hoge | Movies | Videoland</title>
+        <title>{`${detailInfo.title} | Videoland`}</title>
       </Helmet>
       {(isLoading) ? <Loader /> : (
-        <p>Movie detail</p>
+        <Container>
+          <Background imgUrl={detailInfo.backdrop_path !== null ? `https://image.tmdb.org/t/p/original${detailInfo.backdrop_path}` : "#1d1d1d"} />
+          {error && <Message text={error} color="#e50914" />}
+        </Container>
       )}
     </>
   )
