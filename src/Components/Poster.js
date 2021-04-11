b6cf4e33c88e0a5d1bafb423b97624eedda910c1
Hayoung Gam <hahhyoung@gmail.com>
2021-04-11 20:20:32 +0900

    Add dummy image

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -1,18 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div``;
-const Img = styled.img`
-  max-width: 100%;
+const Container = styled(Link)`
+  display: block;
+  position: relative;
 `;
-const Title = styled.h3``;
-const Rate = styled.span``;
 
-function Poster({ title, imgUrl, rate }) {
+const Img = styled.div`
+  width: 100%;
+  height: 210px;
+  background-image: url(${ props => props.imgUrl});
+  background-position: center;
+  background-size: cover;
+`;
+
+const Title = styled.h3`
+  color: #fff;
+`;
+
+const Rate = styled.span`
+  color: #fff;
+`;
+
+function Poster({ id, title, imgUrl, rate }) {
   return (
     <>
-      <Container>
-        <Img src={`https://image.tmdb.org/t/p/w300/${imgUrl}`} />
+      <Container to={`/movie/${id}`}>
+        <Img imgUrl={imgUrl !== null ? `https://image.tmdb.org/t/p/w300${imgUrl}` : require("../assets/noPosterSmall.png").default} />
         <Title>{title}</Title>
         <Rate>{rate}</Rate>
       </Container>
