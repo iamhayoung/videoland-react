d0f7494ba650a4f909770b6a840849fc2296a249
Hayoung Gam <hahhyoung@gmail.com>
2021-04-11 16:35:26 +0900

    Create api setting

---
+++ src/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "b35b41610f8287ba322c0ed02d033aeb",
+    language: "en-US"
+  }
+})
+
+export const moviesApi = {
+  movieDetail: id => api.get(`movie/${id}`, {
+    params: {
+      append_to_response: "videos"
+    }
+  }),
+  nowPlaying: () => api.get("movie/now_playing"),
+  popular: () => api.get("movie/popular"),
+  upcoming: () => api.get("movie/upcoming"),
+  search: text => api.get("search/movie", {
+    params: {
+      query: encodeURIComponent(text)
+    }
+  })
+}
+
+export const tvApi = {
+  tvDetail: id => api.get(`tv/${id}`, {
+    params: {
+      append_to_response: "videos"
+    }
+  }),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  topRated: () => api.get("tv/top_rated"),
+  search: text => api.get("search/tv", {
+    params: {
+      query: encodeURIComponent(text)
+    }
+  })
+}
