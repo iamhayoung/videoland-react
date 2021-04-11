e29e9a169808511855855f20bf96b27c6e773699
Hayoung Gam <hahhyoung@gmail.com>
2021-04-11 15:50:09 +0900

    Create Router, Header

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+  return (
+    <p>header</p>
+  )
+}
+
+export default Header;
---
+++ src/Components/Router.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "Components/Header";
+
+function Sitemap() {
+  return (
+    <Router>
+      <Header />
+    </Router>
+  )
+}
+
+export default Sitemap;
