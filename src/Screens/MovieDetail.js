25c7ade10762d82fdcb4c3d0edf96f1649ede16a
Hayoung Gam <hahhyoung@gmail.com>
2021-04-11 20:20:55 +0900

    Create MovieDetail js

--- src/Screens/MovieDetail.js
+++ src/Screens/MovieDetail.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { moviesApi } from "api";
+import { Helmet } from "react-helmet"
+import Loader from "Components/Loader";
 
-function MovieDetail() {
+function MovieDetail(props) {
+  const [isLoading, setLoading] = useState(true);
+  console.log(props)
+
   return (
-    <p>MovieDetail</p>
+    <>
+      <Helmet>
+        <title>hoge | Movies | Movieland</title>
+      </Helmet>
+      {(isLoading) ? <Loader /> : (
+        <p>Movie detail</p>
+      )}
+    </>
   )
 }
 
