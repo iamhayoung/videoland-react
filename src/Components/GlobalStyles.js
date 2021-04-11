6994b549f75d3e8309eb4c650afd509847869212
Hayoung Gam <hahhyoung@gmail.com>
2021-04-11 15:49:46 +0900

    Create GlobalStyles

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;600&display=swap');
+
+  ${reset};
+  a {
+    text-decoration: none;
+    color: inherit;
+  };
+
+  * {
+    box-sizing: border-box;
+  };
+
+  body {
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  };
+`
+
+export default GlobalStyles;
