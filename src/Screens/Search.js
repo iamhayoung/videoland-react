a8dc1e99151b4b19b9afc03a80328e8f0c34a131
Hayoung Gam <hahhyoung@gmail.com>
2021-04-11 21:56:41 +0900

    Get search results

--- src/Screens/Search.js
+++ src/Screens/Search.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { moviesApi, tvApi } from "api";
+import { Helmet } from "react-helmet"
+import styled from "styled-components";
 
-function Search() {
+const Form = styled.form``;
+const Input = styled.input``;
+
+function Search(props) {
+  const [keyword, setKeyword] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [tvShows, setTVShows] = useState([]);
+  console.log(props)
+  console.log(keyword)
+  console.log(movies);
+  console.log(tvShows);
+
+  function getInputText(event) {
+    const { target: { value } }= event;
+    setKeyword(value);
+  }
+
+  async function getSearchData(event) {
+    event.preventDefault();
+    try {
+      const { data: { results: movieResults} } = await moviesApi.search(keyword);
+      const { data: { results: tvResults} } = await tvApi.search(keyword);
+      setMovies(movieResults);
+      setTVShows(tvResults);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <p>Search</p>
+    <>
+      <Helmet>
+        <title>Search | Videoland</title>
+      </Helmet>
+      <Form onSubmit={getSearchData}>
+        <Input onChange={getInputText} placeholder="Search Movies or TV Shows" />
+      </Form>
+    </>
   )
 }
 
