59ca5ba6902c2d0c5cf87d6ceb3d95f1a251a435
Hayoung Gam <hahhyoung@gmail.com>
2021-04-11 20:40:14 +0900

    Add Poster hover effects

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled(Link)`
+const Piece = styled(Link)`
   display: block;
   position: relative;
 `;
@@ -13,24 +13,54 @@ const Img = styled.div`
   background-image: url(${ props => props.imgUrl});
   background-position: center;
   background-size: cover;
+  transition: .1s linear;
+`;
+
+const Rating = styled.div`
+  position: absolute;
+  bottom: 7px;
+  right: 7px;
+  font-size: 14px;
+  color: #fff;
+  opacity: 0;
+  transition: .1s linear;
+`;
+
+const Rate = styled.span`
+  margin-right: 8px;
+`;
+
+const ImgContainer = styled.div`
+  margin-bottom: 8px;
+  position: relative;
+  overflow: hidden;
+
+  &:hover {
+    ${Img} {
+      opacity: 0.3;
+      transform: scale(1.05);
+    }
+
+    ${Rating} {
+      opacity: 1;
+    }
+  }
 `;
 
 const Title = styled.h3`
   color: #fff;
 `;
 
-const Rate = styled.span`
-  color: #fff;
-`;
-
 function Poster({ id, title, imgUrl, rate }) {
   return (
     <>
-      <Container to={`/movie/${id}`}>
-        <Img imgUrl={imgUrl !== null ? `https://image.tmdb.org/t/p/w300${imgUrl}` : require("../assets/noPosterSmall.png").default} />
+      <Piece to={`/movie/${id}`}>
+        <ImgContainer>
+          <Img imgUrl={imgUrl !== null ? `https://image.tmdb.org/t/p/w300${imgUrl}` : require("../assets/noPosterSmall.png").default} />
+          <Rating><Rate>{rate > 5 ? "🥰" : "🤨"}</Rate>{rate} / 10</Rating>
+        </ImgContainer>
         <Title>{title}</Title>
-        <Rate>{rate}</Rate>
-      </Container>
+      </Piece>
     </>
   )
 }
