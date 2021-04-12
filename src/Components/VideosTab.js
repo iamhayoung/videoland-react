179329b61be2a69c1834a2921b7bb14774d981b3
Hayoung Gam <hahhyoung@gmail.com>
2021-04-12 20:38:10 +0900

    Print out Videos tab

--- src/Components/VideosTab.js
+++ src/Components/VideosTab.js
@@ -1,9 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 40px;
+`;
+
+const Item = styled.div`
+`;
+
+const Player = styled.iframe`
+  width: 100%;
+  max-width: 800px;
+`;
+
+const Name = styled.p`
+  margin-top: 10px;
+  font-weight: 300;
+`;
+
+const Message = styled.p``;
 
 function Video({ videos }) {
   console.log(videos)
   return (
-    <p>111</p>
+    <Container>
+      {videos && videos.length > 0 ?
+        videos.map(video => (
+          <Item key={video.id}>
+            <Player src={`https://www.youtube.com/embed/${video.key}`} height="220px" />
+            <Name>{video.name}</Name>
+          </Item>
+        ))
+        : (<Message>No data😢</Message>)
+      }
+    </Container>
   )
 }
 
