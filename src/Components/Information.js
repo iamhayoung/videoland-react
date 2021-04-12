a0aaaadd84e0944d783326ad141847a7c2192281
Hayoung Gam <hahhyoung@gmail.com>
2021-04-12 15:52:21 +0900

    Edit Detail Layout

--- src/Components/Information.js
+++ src/Components/Information.js
@@ -2,13 +2,47 @@ import React from "react";
 import styled from "styled-components";
 
 const Poster = styled.img`
+  max-width: 35%;
+  width: 100%;
 `;
 
-const Contents = styled.div``;
+const Contents = styled.div`
+  max-width: 65%;
+  width: 100%;
+  margin-left: 50px;
+  flex: 0 1 auto;
+`;
+
+const TitleAndRate = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
 
 const Title = styled.h3`
-  margin-bottom: 20px;
+  width: 80%;
+  font-size: 50px;
+  line-height: 1.2;
+`;
+
+const Rate = styled.div`
+  display: flex;
+  align-items: center;
   font-size: 30px;
+`;
+
+const Spec = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+  color: #c3c2c2;
+`;
+
+const Item = styled.span`
+  font-weight: 300;
+`;
+
+const Divider = styled.span`
+  margin: 0 10px;
 `;
 
 const Genres = styled.ul`
@@ -17,6 +51,7 @@ const Genres = styled.ul`
 
 const Genre = styled.li`
   position: relative;
+  font-weight: 300;
 
   & + & {
     margin-left: 16px;
@@ -29,21 +64,15 @@ const Genre = styled.li`
   }
 `;
 
-const List = styled.ul`
-  display: flex;
+const Description = styled.p`
+  display: -webkit-box;
+  overflow : hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+  font-weight: 300;
+  line-height: 1.6;
 `;
-
-const ListItem = styled.li`
-  position: relative;
-
-  & + & {
-    margin-left: 16px;
-  }
-`;
-
-const Rate = styled.span``;
-
-const Description = styled.p``;
 
 function Information({ title, imgUrl, genres, year, runtime, rate, seasons, episodes, description }) {
   console.log(seasons)
@@ -52,23 +81,29 @@ function Information({ title, imgUrl, genres, year, runtime, rate, seasons, epis
     <>
       <Poster src={imgUrl} />
       <Contents>
-        <Title>{title}</Title>
-        {genres && genres.length > 0 && (
-          <Genres>
-            {genres.map(genre => <Genre key={genre.id}>{genre.name}</Genre>)}
-          </Genres>
-        )}
-        {seasons && (
-          <List>
-            <ListItem>{seasons} Seasons</ListItem>
-            <ListItem>{episodes} Episodes</ListItem>
-          </List>
-        )}
-        <List>
-          <ListItem>{year}</ListItem>
-          <ListItem>{runtime} min</ListItem>
-        </List>
-        <Rate>{rate} / 10</Rate>
+        <TitleAndRate>
+          <Title>{title}</Title>
+          <Rate>{rate} ⭐️</Rate>
+        </TitleAndRate>
+        <Spec>
+          <Item>{year}</Item>
+          <Divider>・</Divider>
+          <Item>{runtime} min</Item>
+          <Divider>・</Divider>
+          {genres && genres.length > 0 && (
+            <Genres>
+              {genres.map(genre => <Genre key={genre.id}>{genre.name}</Genre>)}
+            </Genres>
+          )}
+          {seasons && (
+            <>
+              <Divider>・</Divider>
+              <Item>{seasons} Seasons</Item>
+              <Divider>・</Divider>
+              <Item>{episodes} Episodes</Item>
+            </>
+          )}
+        </Spec>
         <Description>{description}</Description>
       </Contents>
     </>
