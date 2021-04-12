c2cd0dc9e1bca7490d783dd00329d32e9b0517d5
Hayoung Gam <hahhyoung@gmail.com>
2021-04-12 15:12:35 +0900

    Edit IntroduceArea padding, justify-content

--- src/Components/IntroduceArea.js
+++ src/Components/IntroduceArea.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  padding: 50px;
 `;
 
 function IntroduceArea({ children }) {
