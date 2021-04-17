c302d509e7903b8953c38fce811d45fd1a742874
Hayoung Gam <hahhyoung@gmail.com>
2021-04-17 16:54:20 +0900

    Add message proptypes

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Text = styled.p`
   margin: 40px 0;
@@ -11,6 +12,11 @@ function Message({ text, color }) {
   return (
     <Text color={color}>{text}</Text>
   )
+}
+
+Message.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired
 }
 
 export default Message;
